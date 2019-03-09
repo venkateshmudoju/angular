bb13751dd6f305918cc22969708202d87d16204f
venkateshmudoju <mudoju.venkatesh@gmail.com>
2019-03-09 08:31:59 -0700

    Routing basic

---
+++ angular-routing/src/app/settings-contact/settings-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-contact',
+  templateUrl: './settings-contact.component.html',
+  styleUrls: ['./settings-contact.component.scss']
+})
+export class SettingsContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
