1f90b5527f3a811d35684bf3d3c073bc7f7714e5
venkateshmudoju <mudoju.venkatesh@gmail.com>
2019-03-09 07:34:50 -0700

    angular

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    SettingsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
