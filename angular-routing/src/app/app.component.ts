ad31a76d999d3b0bc9f4513b3002624681fbe7f4
venkateshmudoju <mudoju.venkatesh@gmail.com>
2019-03-09 08:56:58 -0700

    Routing with navigation

--- angular-routing/src/app/app.component.ts
+++ angular-routing/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-routing';
+  routes = [
+    {linkName: 'Home', url: 'home'},
+    {linkName: 'Settings', url: 'settings'}
+  ]
 }
