fed382243c90aa8e305be17a50a49b8dc53b554c
venkateshmudoju <mudoju.venkatesh@gmail.com>
2019-03-09 21:30:30 -0700

    Angular examples

--- angular-routing/src/app/employee.service.ts
+++ angular-routing/src/app/employee.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient , HttpErrorResponse} from '@angular/common/http';
+import { HttpClient , HttpErrorResponse, HttpHeaders, HttpParams} from '@angular/common/http';
 import { IEmployee } from './iemployee';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class EmployeeService {
   private _urlGetEmployees = 'http://dummy.restapiexample.com/api/v1/employees1';
   private _urlGetEmployee = 'http://dummy.restapiexample.com/api/v1/employee/';
+
   constructor(private http: HttpClient) { }
   getEmployees(): Observable<IEmployee[]> {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token'
+      })
+    };
+
     return this.http.get<IEmployee[]>(this._urlGetEmployees);
   }
 
@@ -19,8 +31,22 @@ export class EmployeeService {
     return this.http.get<IEmployee>(this._urlGetEmployee).pipe(
       retry(1),
       catchError(this.errorHandler)
-    );;
+    );
   }
+
+  /* GET heroes whose name contains search term */
+searchEmployees(term: string): Observable<IEmployee[]> {
+  term = term.trim();
+
+  // Add safe, URL encoded search parameter if there is a search term
+  const options = term ?
+   { params: new HttpParams().set('name', term) } : {};
+
+  return this.http.get<Hero[]>(this.heroesUrl, options)
+    .pipe(
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+}
 
   errorHandler(error: HttpErrorResponse) {
     //return Observable.throw(error.message);
